de27d1241a665bfcd01ac11f69acd65cf07460d2
mariorich <mariotyuxd@gmail.com>
2025-12-24 12:16:34 -0500

    finished

--- back-end/src/main/java/com/project/code/Service/ServiceClass.java
+++ back-end/src/main/java/com/project/code/Service/ServiceClass.java
@@ -1,26 +1,51 @@
 package com.project.code.Service;
 
+import org.springframework.stereotype.Service;
 
+import com.project.code.Model.Inventory;
+import com.project.code.Model.Product;
+import com.project.code.Model.Store;
+import com.project.code.Repo.InventoryRepository;
+import com.project.code.Repo.ProductRepository;
+
+@Service
 public class ServiceClass {
+    private final InventoryRepository inventoryRepository;
+    private final ProductRepository productRepository;
     
-// 1. **validateInventory Method**:
-//    - Checks if an inventory record exists for a given product and store combination.
-//    - Parameters: `Inventory inventory`
-//    - Return Type: `boolean` (Returns `false` if inventory exists, otherwise `true`)
+    public ServiceClass(InventoryRepository inventoryRepository,ProductRepository productRepository) {
+        this.inventoryRepository = inventoryRepository;
+        this.productRepository=productRepository;
+    }
 
-// 2. **validateProduct Method**:
-//    - Checks if a product exists by its name.
-//    - Parameters: `Product product`
-//    - Return Type: `boolean` (Returns `false` if a product with the same name exists, otherwise `true`)
+    public boolean validateInventory(Inventory inventory){
 
-// 3. **ValidateProductId Method**:
-//    - Checks if a product exists by its ID.
-//    - Parameters: `long id`
-//    - Return Type: `boolean` (Returns `false` if the product does not exist with the given ID, otherwise `true`)
+        Inventory result = inventoryRepository.findByProductIdandStoreId(inventory.getProduct().getId(),inventory.getStore().getId());
+        if(result != null){
+            return false;
+        }
+        return true;
+    }
 
-// 4. **getInventoryId Method**:
-//    - Fetches the inventory record for a given product and store combination.
-//    - Parameters: `Inventory inventory`
-//    - Return Type: `Inventory` (Returns the inventory record for the product-store combination)
+    public Inventory getInventoryId(Inventory inventory){
 
+        Inventory result = inventoryRepository.findByProductIdandStoreId(inventory.getProduct().getId(),inventory.getStore().getId());
+        return result;
+    }
+
+    public boolean validadeProduct(Product product){
+        Product result = productRepository.findByName(product.getName());
+        if(result != null){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validateProduct(Product product){
+        Product result = productRepository.findByid(product.getId);
+        if(result != null){
+            return true;
+        }
+        return false;
+    }
 }
